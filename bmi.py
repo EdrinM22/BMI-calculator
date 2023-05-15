716c6eec0b39aa08b2f8fb6e406334d763fc518f
EdrinM22 <emolla22@epoka.edu.al>
2023-05-15 22:42:01 +0200

    added functions for BMI calculations in metrics and imperial

--- bmi.py
+++ bmi.py
@@ -1,1 +1,17 @@
-print("Bmi")
+import tkinter as tk
+
+def Bmi_Calculator_metrics(weight, height):
+    w = weight
+    h = height
+    return w/h**2
+
+def Bmi_Calculator_Imperial(weightI, heightI):
+    w = weightI
+    h = (heightI**2)
+    return (w/h)*703
+
+
+
+
+root = tk.Tk()
+root.mainloop()
